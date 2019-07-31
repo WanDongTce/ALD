f0f5833e0c536b9ef18e7cd81624c935dbb99694
zhangyk <admin@uimake.cn>
2019-07-31 16:39:49 +0800

    首页 列表
    首页 列表

--- pages/ailangdu/pages/readList/readList.js
+++ pages/ailangdu/pages/readList/readList.js
@@ -1,38 +1,18 @@
-// pages/ailangdu//pages/index/index.js
-const network = require("../../../../utils/main.js");
-const app = getApp();
+// pages/ailangdu//pages/readList/readList.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    imgUrls: [],
-    current: 0
+
   },
-  // swiper
-  change: function(e){
-    console.log(e.detail.current);
-    this.setData({
-      current: e.detail.current
-    });
-  },
-  getSwipImgs: function () {
-    var that = this;
-    //正式使用改成11 爱朗读
-    network.getSwiperImgs(1, function (res) {
-      if (res.data.code == 200) {
-        that.setData({
-          imgUrls: res.data.data[0].list
-        });
-      }
-    });
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getSwipImgs();
+
   },
 
   /**
@@ -67,14 +47,14 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    console.log('onPullDownRefresh');
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    console.log('onReachBottom');
   },
 
   /**
